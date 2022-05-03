32f538a9068bf0059c188d0e3b47f9d090e01100
marmooo <marmooo@users.noreply.github.com>
2022-05-04 08:58:48 +0900

    update db

--- reduce.py
+++ reduce.py
@@ -1,14 +1,15 @@
 words = {}
-with open("words.lst", "w") as fout:
-    count = 0
-    with open("mGSL/dist/mGSL.lst") as fin:
-        for line in fin:
+with open("mGSL/dist/mGSL.lst") as f:
+    for line in f:
+        word = line.split("\t", 1)[0]
+        words[word] = True
+
+count = 0
+with open("crawl-300d-2M.vec") as f:
+    for line in f:
+        word = line.split(" ", 1)[0]
+        if word in words:
             count += 1
-            if count > 10000:
-                break
-            word = line.split("\t", 1)[0]
-            words[word] = True
-            fout.write(word + "\n")
 
 with open("crawl-300d-2M-small.vec", "w") as fout:
     fout.write(str(len(words)) + " 300\n")
