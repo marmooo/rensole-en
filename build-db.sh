97b681622000d9743cd4dfe55b6f751d5f52410a
marmooo <marmooo@users.noreply.github.com>
2023-06-21 10:11:23 +0900

    improve

--- build-db.sh
+++ build-db.sh
@@ -1,6 +1,6 @@
 rm remote.db
 rm -rf docs/db
-cp crawl-300d-2M-small.magnitude remote.db
+mv crawl-300d-2M-small.magnitude remote.db
 bash optimize.sh
 mkdir -p docs/db
 bash create_db.sh remote.db docs/db
